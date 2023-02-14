02691f6f383ca36137ed3eab637393eb4fbf72ab
Ihovanna <90429068+Ihovanna@users.noreply.github.com>
2023-02-14 15:33:20 -0800

    changed a few things so as to make changed be pushed to github

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+
+const firebaseKey = process.env.REACT_APP_FIREBASE;
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE,
+  apiKey: firebaseKey,
   authDomain: "hurricane-agatha-2022-map.firebaseapp.com",
   projectId: "hurricane-agatha-2022-map",
   storageBucket: "hurricane-agatha-2022-map.appspot.com",
   messagingSenderId: "903213746797",
   appId: "1:903213746797:web:352fb2815e6823f1f18d64",
-  measurementId: "G-LX37NYKW0W"
+  measurementId: "G-LX37NYKW0W",
 };
 
 const app = initializeApp(firebaseConfig);
